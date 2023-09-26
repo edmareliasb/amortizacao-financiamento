18c575de6bd36c08f52684be05c2e030a89ec75e
edmarelias25 <edmarelias25@yahoo.com.br>
2023-09-26 08:46:09 -0300

    incorporando validacao do campo tabela

--- src/infra/routes/middleware/validarBodyGeraParcelar.ts
+++ src/infra/routes/middleware/validarBodyGeraParcelar.ts
@@ -1,14 +1,15 @@
 import { NextFunction, Request, Response } from "express";
 
 export function validarCorpoDaSolicitacao(req: Request, res: Response, next: NextFunction) {
-    const {valorFinanciamento, valorEntrada, taxaJuros, periodo} = req.body;
+    const {valorFinanciamento, valorEntrada, taxaJuros, periodo, tabela} = req.body;
   
     // Verificar se todos os campos obrigatórios estão presentes
     if (
       valorFinanciamento === undefined ||
       valorEntrada === undefined ||
       taxaJuros === undefined ||
-      periodo === undefined
+      periodo === undefined ||
+      tabela == undefined
     ) {
       return res.status(400).json({ error: 'Todos os campos são obrigatórios.' });
     }
@@ -18,9 +19,15 @@ export function validarCorpoDaSolicitacao(req: Request, res: Response, next: Nex
       typeof valorFinanciamento !== 'number' ||
       typeof valorEntrada !== 'number' ||
       typeof taxaJuros !== 'number' ||
-      typeof periodo !== 'number'
+      typeof periodo !== 'number' ||
+      typeof tabela !== 'string'
     ) {
       return res.status(400).json({ error: 'Tipos de campo inválidos.' });
+    }
+
+    if (tabela.toUpperCase() != 'PRICE' || 
+        tabela.toUpperCase() != 'SAC') {
+          return res.status(400).json({ error: 'Valor inválido para campo Tabela. Informe PRICE ou SAC.' });
     }
   
     // Se todas as validações passarem, chame o próximo middleware ou a função de rota principal
